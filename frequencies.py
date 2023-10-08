3fb09929127f3a3845e0055e2a947970806c94ba
Luay Boettgenbach <luay.boettgenbach@kcl.ac.uk>
2023-10-08 13:01:32 +0100

    Convert items in the array to strings and remove duplicates.

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,17 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    stringItems = []
+
+    # Convert items in the array to strings
+    for item in items:
+        itemToAppend = str(item)
+        stringItems.append(itemToAppend)
+    
+    # Remove duplicates from the stringItems array
+    stringItems = list(dict.fromkeys(stringItems))
+
+
+
+
     return frequencies
