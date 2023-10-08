37a6944a95932d8168d075f9ccd6f71ee271347d
Luay Boettgenbach <luay.boettgenbach@kcl.ac.uk>
2023-10-08 13:35:40 +0100

    Add frequencies to dictionary.

--- frequencies.py
+++ frequencies.py
@@ -1,20 +1,23 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    stringItems = []
+  frequencies = {}
+  # Your code goes here
+  stringItems = []
 
-    # Convert items in the array to strings
-    for item in items:
-        itemToAppend = str(item)
-        stringItems.append(itemToAppend)
-    
-    # Remove duplicates from the stringItems array
-    stringItems = list(dict.fromkeys(stringItems))
+  # Convert items in the array to strings
+  for item in items:
+    itemToAppend = str(item)
+    stringItems.append(itemToAppend)
 
+  # Remove duplicates from the stringItems array
+  cleanItems = list(dict.fromkeys(stringItems))
 
+  # Add to the dictionary
+  for string in cleanItems:
+    value = stringItems.count(string)
+    frequencies[string] = value
 
-
-    return frequencies
+  return frequencies
